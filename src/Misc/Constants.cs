af07b4d2e5ae4cf3c55de5ec528437410a2691e2
GreenComfyTea <klunics97@gmail.com>
2025-03-24 21:37:11 +0200

    chore: Bump version to v1.2.3

--- src/Misc/Constants.cs
+++ src/Misc/Constants.cs
@@ -9,7 +9,7 @@ internal static class Constants
 	public const string ModName = "YURI Overlay";
 	public const string ModNameNoWhitespaces = "YURI_Overlay";
 
-	public const string Version = "1.2.2";
+	public const string Version = "1.2.3";
 
 	public const string DataPath = @"reframework\data\";
 	public const string PluginDataPath = $@"{DataPath}{ModNameNoWhitespaces}\";
