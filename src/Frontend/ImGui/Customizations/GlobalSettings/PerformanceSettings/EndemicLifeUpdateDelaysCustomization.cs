8e192b1758ad57feb1f96d4780072feec3c83041
GreenComfyTea <klunics97@gmail.com>
2025-05-01 09:28:48 +0300

    fix: Fixed Endemic Life Update Delays customization

--- src/Frontend/ImGui/Customizations/GlobalSettings/PerformanceSettings/EndemicLifeUpdateDelaysCustomization.cs
+++ src/Frontend/ImGui/Customizations/GlobalSettings/PerformanceSettings/EndemicLifeUpdateDelaysCustomization.cs
@@ -6,20 +6,18 @@ internal sealed class EndemicLifeUpdateDelaysCustomization : Customization
 {
 	public float Name = 5f;
 	public float ModelRadius = 5f;
-	public float DynamicList = 0.2f;
 
 	public bool RenderImGui(string parentName = "", EndemicLifeUpdateDelaysCustomization defaultCustomization = null)
 	{
 		var localization = LocalizationManager.Instance.ActiveLocalization.Data.ImGui;
 
 		var isChanged = false;
-		var customizationName = $"{parentName}-large-monsters";
+		var customizationName = $"{parentName}-endemic-life";
 
-		if(ImGuiHelper.ResettableTreeNode(localization.SmallMonsters, customizationName, ref isChanged, defaultCustomization, Reset))
+		if(ImGuiHelper.ResettableTreeNode(localization.EndemicLife, customizationName, ref isChanged, defaultCustomization, Reset))
 		{
 			isChanged |= ImGuiHelper.ResettableDragFloat($"{localization.Name}##{customizationName}", ref Name, 0.001f, 0.001f, 10f, "%.3f", defaultCustomization?.Name);
 			isChanged |= ImGuiHelper.ResettableDragFloat($"{localization.ModelRadius}##{customizationName}", ref ModelRadius, 0.001f, 0.001f, 10f, "%.3f", defaultCustomization?.ModelRadius);
-			isChanged |= ImGuiHelper.ResettableDragFloat($"{localization.DynamicList}##{customizationName}", ref DynamicList, 0.001f, 0.001f, 10f, "%.3f", defaultCustomization?.DynamicList);
 
 			ImGui.TreePop();
 		}
@@ -33,6 +31,5 @@ internal sealed class EndemicLifeUpdateDelaysCustomization : Customization
 
 		Name = defaultCustomization.Name;
 		ModelRadius = defaultCustomization.ModelRadius;
-		DynamicList = defaultCustomization.DynamicList;
 	}
 }
